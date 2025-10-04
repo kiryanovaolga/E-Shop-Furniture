e94b2b107f3c2adc6ea1bc140145e407980de427
kiryanovaolga <olgakiryanova55@yandex.ru>
2025-10-04 11:10:09 +0200

    The product filter on the site page and url encode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,4 +1,5 @@
 from atexit import register
+from urllib.parse import urlencode
 from django import template
 from goods.models import Categories
 
@@ -10,3 +11,21 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+
+# Этот тег берёт текущие GET-параметры, обновляет их новыми значениями и возвращает строку для URL,
+# чтобы при пагинации сохранялись фильтры и сортировка.
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context["request"].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
+
+
+"""
+urlencode
+превращает словарь параметров в строку GET-параметров, пригодную для URL()
+params = {"on_sale": "on", "page": 2, "order_by": "price"}
+urlencode(params) = 'on_sale=on&page=2&order_by=price'
+
+"""
--- goods/views.py
+++ goods/views.py
@@ -8,6 +8,8 @@ from goods.models import Products
 
 def catalog(request, category_slug):
     page = request.GET.get("page", 1)
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
 
     if category_slug == "all-categories":
         goods = Products.objects.all()
@@ -15,6 +17,11 @@ def catalog(request, category_slug):
         # найди все Products, у которых связанная category имеет поле slug, равное значению переменной category_slug» или 404
         # Пример: фильтруются товары, чья категория имеет slug == 'living-room'.
         goods = get_list_or_404(Products, category__slug=category_slug)
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)  # Разбить goods на страницы по 3 товара
     current_page = paginator.page(int(page))  # Получаем текущую страницу
