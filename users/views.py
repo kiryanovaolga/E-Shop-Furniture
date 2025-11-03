b521c4ba5035743c2acf3fbea6d66b7396fd55c8
kiryanovaolga <olgakiryanova55@yandex.ru>
2025-11-03 11:04:24 +0100

    Profile form controller and notifications are ready

--- users/views.py
+++ users/views.py
@@ -1,11 +1,13 @@
-from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from profile import Profile
+from django.contrib import auth, messages
 from django.contrib.auth import authenticate
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.template import context
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -23,6 +25,7 @@ def login(request):
             if user:  # Проверяем, действительно ли пользователь найден
                 # выполняет вход пользователя на сайт, сохраняя информацию о нём в сессии.
                 auth.login(request, user)
+                messages.success(request, f"{username}, you successfully logged in!")
                 return HttpResponseRedirect(reverse("main:index"))
 
     # Если запрос не POST, то отображаем опять пустую форму на этой же странице
@@ -43,6 +46,9 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(
+                request, f"{user.username}, you successfully registrated and logged in!"
+            )
             return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserRegistrationForm()
@@ -51,13 +57,31 @@ def registration(request):
     return render(request, "users/registration.html", context)
 
 
+@login_required
 def profile(request):
+    if request.method == "POST":
+        form = ProfileForm(
+            data=request.POST, instance=request.user, files=request.FILES
+        )  # при загрузке файла, браузер отправляет его не в POST, а в отдельную структуру FILES.
+        if form.is_valid():
+            form.save()
+            messages.success(request, f"Your profile has been successfully updated!")
+            return HttpResponseRedirect(reverse("user:profile"))
+    else:
+        form = ProfileForm(
+            instance=request.user
+        )  # Создаем форму профиля, заполняя её текущими данными авторизованного пользователя.
     context = {
         "title": "Home - Profile",
+        "form": form,
     }
     return render(request, "users/profile.html", context)
 
 
+@login_required
 def logout(request):
+    messages.success(
+        request, f"{request.user.username}, you have successfully logged out."
+    )
     auth.logout(request)
     return redirect(reverse("main:index"))
